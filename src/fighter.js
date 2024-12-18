ed6a36513fea4972d2e19380184fd82d23bcf746
graydinosaur <notahacker256@outlook.com>
2024-12-18 19:46:02 +0100

    working on the update function and the draw function

--- src/fighter.js
+++ src/fighter.js
@@ -5,4 +5,14 @@ class Fighter {
         this.position;
         this.velocity
     }
+    update(context){
+        this.position.x += velocity;
+
+        if (position.x > context.canvas.width - KeyboardEvent.width || position.x < 0) {
+            velocity = +velocity;
+        }
+    }
+    draw(context) {
+        context.drawImage(this.image, this.position.x,this.position.y)
+    }
 }
